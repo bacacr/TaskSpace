93f80c42ca7c5e11e44d3f9f500e00f1e02cf169
Administrator <Administrator@DESKTOP-099UQRG>
2019-09-03 14:56:27 -0400

    Initial Commit of Spring Boot Project

---
+++ src/main/java/com/plantplaces/PlantplacesApplication.java
@@ -0,0 +1,13 @@
+package com.plantplaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantplacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantplacesApplication.class, args);
+	}
+
+}
