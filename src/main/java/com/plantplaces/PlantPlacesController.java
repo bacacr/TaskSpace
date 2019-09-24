435e66cc5a5f6775c15490acad567a13588a6155
Administrator <Administrator@DESKTOP-099UQRG>
2019-09-24 16:43:53 -0400

    Changed \start method and added GET request mapping for start/json header, loyalty=blue, and loyalty=silver methods for testing with Postman.  readJSON, readBlue, and readSilver methods added.

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -1,7 +1,9 @@
 package com.plantplaces;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @author Administrator
@@ -11,8 +13,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PlantPlacesController {
 	
 	
-	@RequestMapping("/start")
-	public String start() {
+	@RequestMapping(value = "/start", method = RequestMethod.GET)
+	public String read() {
+		
+		return "start";
+	}
+	
+	@RequestMapping(value = "/start", method = RequestMethod.GET, params = {"loyalty=blue"})
+	public String readBlue() {
+		
+		return "start";
+	}
+	
+	@RequestMapping(value = "/start", method = RequestMethod.GET, params = {"loyalty=silver"})
+	public String readSilver() {
+		
+		return "start";
+	}
+	
+	@RequestMapping(value = "/start", method = RequestMethod.GET, headers = {"content-type=text/json"})
+	public String readJSON() {
+		
+		return "start";
+	}
+	
+	@PostMapping("/start")
+	public String create() {
 		
 		return "start";
 	}
