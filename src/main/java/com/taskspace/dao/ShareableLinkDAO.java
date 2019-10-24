e3d5ec8e6c7621363a694716cf25ebc5b5487ad8
colinbaca <colinbaca@colins-mbp.wifi.uc.edu>
2019-10-24 14:49:46 -0400

    Added Some Sharable Link Data

--- src/main/java/com/taskspace/dao/ShareableLinkDAO.java
+++ src/main/java/com/taskspace/dao/ShareableLinkDAO.java
@@ -1,5 +1,14 @@
 package com.taskspace.dao;
 
 public class ShareableLinkDAO {
+	
+	/**
+	 * @author Bacacr
+	 * Setting baseURL & relativeURL
+	 */
+
+	URL baseURL = new URL("http://www.TaskSpace.com/");
+	
+	URL relativeURL = new URL(baseURL, projectName);
 
 }
